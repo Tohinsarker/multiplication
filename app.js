f53278ca409e6279595eee989ad296b7b1fb3948
Tohin Sarker <tohinsarker55@gmail.com>
2022-11-24 20:01:27 +0600

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+const number = Math.floor(Math.random()*10);
+const number2 = Math.floor(Math.random()*10);
+
+const ques1 = document.getElementById('question1');
+ques1.innerHTML = number;
+
+const ques2 = document.getElementById('question2');
+ques2.innerHTML=  number2;
